198dc396543c3bb2065e78c8233012e7926613fd
Eegii219 <Enkhtsetseg.Unurbayar@tachconsulting.tech>
2024-11-07 20:54:10 -0500

    first page done

---
+++ FirstUI/FirstUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstUI
+//
+//  Created by Enkhtsetseg Unurbayar on 11/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
